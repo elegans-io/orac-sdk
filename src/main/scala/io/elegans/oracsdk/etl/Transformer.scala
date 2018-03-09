f2f9c186cf1e76cccea1845b6e7d6b40281bea0c
Mario Alemi <mario.alemi@gmail.com>
2018-03-09 20:56:14 +0100

    updated makeRankId

--- src/main/scala/io/elegans/oracsdk/etl/Transformer.scala
+++ src/main/scala/io/elegans/oracsdk/etl/Transformer.scala
@@ -38,41 +38,63 @@ class Transformer {
   }
 
   /**
-    * Substitute elements in a column with a numerical ID inversely
+    * Substitute elements in one or more columns with a numerical ID inversely
     * proportional to occurrence. Eg if RDD contains rows of books,
-    * and the 3rd column is "author", SimpleRankID will substitute any
-    * author with a Int.toString where "0" is the most popular author,
+    * and the 3rd column is "author" and the 4th "title", it will add a column
+    * an Int.toString where "0" is the most popular book author+title
     * "1" the second most popular etc.
-    * If replace, updates the array, otherwise appends
+    * (different books with same author+title will have same RankID)
+    *
+    * @param input arrays of fields (all strings)
+    * @param columns identifiers for the rank
+    * @param tokenize tokenize the resulting string and substitute RankIDs to tokens (words)
+    * @return same RDD with one more column with the RankIDs
     *
     */
-  def makeRankId(input: RDD[Array[String]], column: Int, replace: Boolean, tokenize: Boolean ): RDD[Array[String]] = {
+  def makeRankId(input: RDD[Array[String]], columns: Seq[Int],
+                 tokenize: Boolean,
+                 replace:Option[Int]=None): RDD[Array[String]] = {
 
-    if (replace) {
-      if (!tokenize) {
-        val rankid_map = input.map(x => (x(column), 1)).reduceByKey((a, b) => a + b).sortBy(- _._2).map(x => x._1).zipWithIndex.map(x => (x._1, x._2.toString)).collect.toMap
-        input.map(x => x.updated(column, rankid_map(x(column))))
-      } else {
-        val input_with_tokenized_column = input.map(x => x.updated(column, this.tokenizer(x(column))))
-        // from String to clean List[String]
-        //val tokenized_column = input.map(x => OracClientTokenizer(x(column)))
-        val tokenized_column = input.map(x => x(column))
-        // makes the map (Word, rankid)
-        val rankid_map = tokenized_column.flatMap(line => line.split(" ")).map(x => (x, 1)).reduceByKey((a, b) => a + b).sortBy(- _._2).map(x => x._1).zipWithIndex.map(x => (x._1, x._2.toString)).collect.toMap
-        input_with_tokenized_column.map(x => x.updated(column, x(column).split(" ").map(y => rankid_map.getOrElse(y, "")).mkString(", ")))
+    assert(input.first.length >= columns.max + 1)
+    val new_input = input.map{x => x :+ columns.map(y => x(y)).mkString(" ")}
+
+    if (!tokenize) {
+      // token => rankid
+      val rankid_map = new_input.map(x => (x.last, 1)).reduceByKey((a, b) => a + b)
+        .sortBy(- _._2).map(x => x._1).zipWithIndex
+        .map(x => (x._1, x._2.toString)).collect.toMap
+      replace match {
+        case Some(v) => new_input.map(x => x.dropRight(1) :+ rankid_map(x.last)).map(x => x.updated(v, x.last).dropRight(1))
+        case _ => new_input.map(x => x.dropRight(1) :+ rankid_map(x.last))
       }
-    }
-    else {
-      val rankid_map = input.map(x => (x(column), 1)).reduceByKey((a, b) => a + b).sortBy(- _._2).map(x => x._1).zipWithIndex.map(x => (x._1, x._2.toString)).collect.toMap
-      input.map(x => x ++ Array(rankid_map(x(column))))
+    } else {
+      val input_with_tokenized_column = new_input.map(x => x :+ this.tokenizer(x.last))
+      // from String to clean List[String]
+      //val tokenized_column = input.map(x => OracClientTokenizer(x(column)))
+      val tokenized_column = new_input.map(x => x.last)
+      // makes the map (Word, rankid)
+      val rankid_map = tokenized_column.flatMap(line => line.split(" "))
+        .map(x => (x, 1)).reduceByKey((a, b) => a + b)
+        .sortBy(- _._2).map(x => x._1)
+        .zipWithIndex.map(x => (x._1, x._2.toString)).collect.toMap
+      replace match {
+        case Some(v) => input_with_tokenized_column.map(x => x.dropRight(1) :+ x.last.split(" ")
+          .map(y => rankid_map.getOrElse(y, ""))
+          .mkString(" ")
+        ).map(x => x.updated(v, x.last).dropRight(1))
+        case _ => input_with_tokenized_column.map(x => x.dropRight(1) :+ x.last.split(" ")
+          .map(y => rankid_map.getOrElse(y, ""))
+          .mkString(" ")
+        )
+      }
     }
   }
 
 
   /** Join two RDD[Array[String]]
-    * rdd1 = [username, action_date, ...]
-    * rdd2 = [username, address, user_id]
-    * val rdd3 = Join(input=rdd1, input_column=0, table=rdd2, lookup_columns=(0, 2), replace=true)
+    * input = [username, action_date, ...]
+    * table = [username, address, user_id]
+    * val rdd3 = Join(input=input, input_column=0, table=table, key_column=0, value_column=2, replace=true)
     * rdd3: [user_id, action_date, ...]
     *
     * If !replace append the result
@@ -83,11 +105,16 @@ class Transformer {
                      replace: Boolean
                     ): RDD[Array[String]] = {
     if (replace)
-      input.map(x => (x(input_column), x)).join(table.map(x => (x(key_column), x(value_column)))).map(x => x._2._1.updated(input_column, x._2._2))
+      input.map(x => (x(input_column), x))
+        .join(
+          table.map(x => Tuple2(x(key_column), x(value_column)))
+        )
+        .map(x => x._2._1.updated(input_column, x._2._2))
     else
-      input.map(x => (x(input_column), x)).join(table.map(x => (x(key_column), x(value_column)))).map(x => x._2._1 :+ x._2._2)
+      input.map(x => (x(input_column), x))
+        .join(table.map(x => Tuple2(x(key_column), x(value_column))))
+        .map(x => x._2._1 :+ x._2._2)
   }
-
 }
 
 
