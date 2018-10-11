48e3319eb099755b6ab20dd177a136263a2d6af3
Angelo Leto <angleto@gmail.com>
2018-10-11 14:37:39 +0200

    refactoring cleantextpairs

--- src/main/scala/io/elegans/oracsdk/commands/CleanTextPairs.scala
+++ src/main/scala/io/elegans/oracsdk/commands/CleanTextPairs.scala
@@ -1,11 +1,10 @@
 package io.elegans.oracsdk.commands
 
+import io.elegans.oracsdk.tools.TextProcessingUtils
 import org.apache.spark.sql.SparkSession
 import scopt.OptionParser
-import io.elegans.oracsdk.tools.TextProcessingUtils
-import org.apache.spark.broadcast.Broadcast
 
-/** Read a list of CVs and JobDescriptions, clean and prepare the dataset */
+/** Read a list of Text1 and Text2, clean and prepare the dataset */
 object CleanTextPairs {
 
   implicit class Crossable[X](xs: Traversable[X]) {
@@ -14,17 +13,17 @@ object CleanTextPairs {
 
   /** Case class for command line variables with default values
     *
-    * @param cvfile the users data
-    * @param jdfile the items data
+    * @param text1file the users data
+    * @param text2file the items data
     * @param dictionary the word dictionary used to split words
     * @param output the path for output data
     */
   private case class Params(
-                             cvfile: String = "CVs.tsv",
-                             jdfile: String = "JDs.tsv",
+                             text1file: String = "text1.tsv",
+                             text2file: String = "text2.tsv",
                              dictionary: String = "dictionary.txt",
                              languages: List[String] = List("en", "it"),
-                             output: String = "CVs_DATASET"
+                             output: String = "T1T2_DATASET"
                            )
 
   /** Do all the spark initialization, the logic of the program is here
@@ -34,27 +33,26 @@ object CleanTextPairs {
   private def doEtl(params: Params) {
     val spark = SparkSession.builder().appName("CleanTextPairs").getOrCreate()
 
-    val cvData = spark.read.format("csv").
+    val t1Data = spark.read.format("csv").
       option("header", "true").
       option("delimiter", "\t").
-      load(params.cvfile)
-    cvData.createOrReplaceTempView("CVs")
+      load(params.text1file)
+    t1Data.createOrReplaceTempView("Text1")
 
-    val jdData = spark.read.format("csv").
+    val t2Data = spark.read.format("csv").
       option("header", "true").
       option("delimiter", "\t").
-      load(params.jdfile)
-    jdData.createOrReplaceTempView("JDs")
+      load(params.text2file)
+    t2Data.createOrReplaceTempView("Text2")
 
     val dictionary = spark.sparkContext.textFile(params.dictionary).map{
       case (w) => (w, 0)
     }.collectAsMap()
 
     val mergedData = spark.sql(
-      //firstname, lastname, email, jd, cv
-      "SELECT cv.`User Firstname`, cv.`User Lastname`, cv.`User E-mail Address`, " +
-        "jd.`Req #`, jd.`Requisition Title Translated`, jd.`Requisition Description Translated`, cv.`Job Seeker Resume Body`" +
-        " FROM CVs cv JOIN JDs jd ON cv.`Req #` = jd.`Req #`")
+      "SELECT t1.`User Firstname`, t1.`User Lastname`, t1.`User E-mail Address`, " +
+        "t2.`Req #`, t2.`Requisition Title Translated`, t2.`Requisition Description Translated`, t1.`Job Seeker Resume Body`" +
+        " FROM Text1 t1 JOIN Text2 jd ON t1.`Req #` = t2.`Req #`")
 
     val stopWords = spark.sparkContext.broadcast(Set[String]())
 
@@ -62,20 +60,19 @@ object CleanTextPairs {
       (row.getString(6), row.getString(4) + " " + row.getString(5))
     }
 
-    val tokenizedTextRaw = textPairs.map { case (cv, jd) =>
-      val cvTokens = TextProcessingUtils.tokenizeSentence(text = cv, stopWords = stopWords)
-      val jdTokens = TextProcessingUtils.tokenizeSentence(text = jd, stopWords = stopWords)
-      (cv, jd, cvTokens, jdTokens)
+    val tokenizedTextRaw = textPairs.map { case (t1, t2) =>
+      val T1Tokens = TextProcessingUtils.tokenizeSentence(text = t1, stopWords = stopWords)
+      val T2Tokens = TextProcessingUtils.tokenizeSentence(text = t2, stopWords = stopWords)
+      (t1, t2, T1Tokens, T2Tokens)
     }
 
-    val filterLang = params.languages.toSet
     val tokenizedText = tokenizedTextRaw
 
-    val mergedCvJdTokens = tokenizedText.flatMap{case (_, _, cvTok, jdTok) => cvTok ++ jdTok}
+    val mergedT1T2Tokens = tokenizedText.flatMap{case (_, _, t1Tok, t2Tok) => t1Tok ++ t2Tok}
 
-    val mergedCvJdTokensWordCount = mergedCvJdTokens.map(token => (token, 1)).reduceByKey(_ + _).collectAsMap()
+    val mergedT1T2TokensWordCount = mergedT1T2Tokens.map(token => (token, 1)).reduceByKey(_ + _).collectAsMap()
 
-    val outOfDictionaryTokens = mergedCvJdTokens.map { case(word) =>
+    val outOfDictionaryTokens = mergedT1T2Tokens.map { case(word) =>
       dictionary.contains(word) match {
         case false =>
           val candidates = (1 to word.length).map{ case(i) => word.slice(0, i) }.map { case(firstChunk) =>
@@ -88,8 +85,8 @@ object CleanTextPairs {
           }
           val replacement = candidates.map{
             case((origToken,(firstPart, secondPart))) =>
-              val count = mergedCvJdTokensWordCount.getOrElse(firstPart, 0) +
-                mergedCvJdTokensWordCount.getOrElse(secondPart, 0)
+              val count = mergedT1T2TokensWordCount.getOrElse(firstPart, 0) +
+                mergedT1T2TokensWordCount.getOrElse(secondPart, 0)
               (origToken,(firstPart, secondPart, count))
           }.maxBy(_._2._3)
           (replacement._1, (replacement._2._1, replacement._2._2))
@@ -103,8 +100,8 @@ object CleanTextPairs {
     val outOfDictionaryTokensMap = outOfDictionaryTokens.collectAsMap
 
     val tokenizedTextReplacedTokens = tokenizedText.map{
-      case (cv, jd, cvTokens, jdTokens) =>
-        val newCvTokens = cvTokens.map(t => List(t)).flatMap { case(token) =>
+      case (t1, t2, t1Tokens, t2Tokens) =>
+        val newT1Tokens = t1Tokens.map(t => List(t)).flatMap { case(token) =>
           outOfDictionaryTokensMap.get(token.head) match {
             case Some(t) =>
               List(t._1, t._2)
@@ -112,19 +109,19 @@ object CleanTextPairs {
               token
           }
         }
-        (cv, jd,
-          newCvTokens.filter(_ != ""),
-          jdTokens.filter(_ != ""))
+        (t1, t2,
+          newT1Tokens.filter(_ != ""),
+          t2Tokens.filter(_ != ""))
     }
 
     tokenizedTextReplacedTokens.map {
-      case((_, _, newCvTokens, jdTokens)) =>
-        newCvTokens.mkString(",") + "\t" + jdTokens.mkString(",")
-    }.saveAsTextFile(params.output + "/DATASET_CV_JD")
+      case((_, _, newT1Tokens, t2Tokens)) =>
+        newT1Tokens.mkString(",") + "\t" + t2Tokens.mkString(",")
+    }.saveAsTextFile(params.output + "/DATASET_T1_T2")
 
     tokenizedTextReplacedTokens.map {
-      case((_, _, newCvTokens, jdTokens)) =>
-        newCvTokens ++ jdTokens
+      case((_, _, newT1Tokens, t2Tokens)) =>
+        newT1Tokens ++ t2Tokens
     }.flatMap(list => list).map(token => (token, 1))
       .reduceByKey(_ + _)
       .sortBy(_._2, ascending=false)
@@ -145,14 +142,14 @@ object CleanTextPairs {
     val parser = new OptionParser[Params]("CleanTextPairs") { /* initialization of the parser */
       head("create sentences from user iteractions list of sentences with spark")
       help("help").text("prints this usage text")
-      opt[String]("cvfile")
-        .text(s"the CVs data file" +
-          s"  default: ${defaultParams.cvfile}")
-        .action((x, c) => c.copy(cvfile = x))
-      opt[String]("jdfile")
-        .text(s"the JDs data file" +
-          s"  default: ${defaultParams.jdfile}")
-        .action((x, c) => c.copy(jdfile = x))
+      opt[String]("text1file")
+        .text(s"the first text data file" +
+          s"  default: ${defaultParams.text1file}")
+        .action((x, c) => c.copy(text1file = x))
+      opt[String]("text2file")
+        .text(s"the second text data file" +
+          s"  default: ${defaultParams.text2file}")
+        .action((x, c) => c.copy(text2file = x))
       opt[String]("dictionary")
         .text(s"the words dictionary file" +
           s"  default: ${defaultParams.dictionary}")
